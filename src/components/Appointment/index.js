6a79b6529516bb5bc59ea7164d990f2d3f8bd6c3
lianaa98 <liana.kan98@gmail.com>
2023-05-25 15:10:44 -0700

    wrote test for delete

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -25,13 +25,14 @@ export default function Appointment(props) {
   const ERROR_SAVE = "ERROR_SAVE";
   const ERROR_DELETE = "ERROR_DELETE";
 
-
-  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   function save(name, interviewer) {
     const interview = {
       student: name,
-      interviewer
+      interviewer,
     };
 
     transition(SAVING);
@@ -39,7 +40,7 @@ export default function Appointment(props) {
     props
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
-      .catch(error => transition(ERROR_SAVE, true));
+      .catch((error) => transition(ERROR_SAVE, true));
   }
 
   function destroy() {
@@ -47,17 +48,13 @@ export default function Appointment(props) {
     props
       .cancelInterview(props.id)
       .then(() => transition(EMPTY))
-      .catch(error => transition(ERROR_DELETE, true));
+      .catch((error) => transition(ERROR_DELETE, true));
   }
-
 
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && (
-        <Empty onAdd={() => transition(CREATE)}
-        />
-      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
@@ -90,6 +87,7 @@ export default function Appointment(props) {
           message="Are you sure you would like to delete?"
           onCancel={back}
           onConfirm={destroy}
+          data-testid="confirm"
         />
       )}
       {mode === ERROR_SAVE && (
@@ -98,7 +96,6 @@ export default function Appointment(props) {
       {mode === ERROR_DELETE && (
         <Error message="Could not cancel appointment." onClose={back} />
       )}
-
     </article>
   );
 }
